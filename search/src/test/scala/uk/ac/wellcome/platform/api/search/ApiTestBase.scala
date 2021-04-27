6ff4366492aa6dde593c945ac91161542d682cbe
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-04-27 15:32:18 +0000

    Apply auto-formatting rules

--- search/src/test/scala/uk/ac/wellcome/platform/api/search/ApiTestBase.scala
+++ search/src/test/scala/uk/ac/wellcome/platform/api/search/ApiTestBase.scala
@@ -4,7 +4,7 @@ import com.sksamuel.elastic4s.{ElasticDsl, Index}
 import com.sksamuel.elastic4s.ElasticDsl._
 import org.scalatest.Assertion
 import uk.ac.wellcome.display.models.ApiVersions
-import uk.ac.wellcome.fixtures.{Fixture, RandomGenerators, fixture}
+import uk.ac.wellcome.fixtures.{fixture, Fixture, RandomGenerators}
 import uk.ac.wellcome.platform.api.search.fixtures.ApiFixture
 
 trait ApiTestBase extends ApiFixture with RandomGenerators {
