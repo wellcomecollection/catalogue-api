2ba174c98b213dc62c300afbe6cd1f346178c18b
Robert Kenny <kenoir@gmail.com>
2021-04-30 10:30:50 +0100

    Revert "remove items app changes from branch"
    This reverts commit 45ab9f49ee8cf2e0d1863fa6dc14ac67b7df2322.

--- items/src/main/scala/uk/ac/wellcome/platform/api/items/Main.scala
+++ items/src/main/scala/uk/ac/wellcome/platform/api/items/Main.scala
@@ -9,9 +9,11 @@ import uk.ac.wellcome.platform.api.common.http.WellcomeHttpApp
 import uk.ac.wellcome.platform.api.common.http.config.builders.HTTPServerBuilder
 import uk.ac.wellcome.platform.api.common.services.StacksService
 import uk.ac.wellcome.platform.api.common.services.config.builders.StacksServiceBuilder
+import uk.ac.wellcome.platform.api.models.ApiConfig
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
 import weco.http.monitoring.HttpMetrics
+import uk.ac.wellcome.typesafe.config.builders.EnrichConfig.RichConfig
 
 import scala.concurrent.ExecutionContext
 
@@ -25,9 +27,30 @@ object Main extends WellcomeTypesafeApp {
 
     val workService: StacksService = StacksServiceBuilder.build(config)
 
+    val apiConf =
+      ApiConfig(
+        host = config
+          .getStringOption("api.host")
+          .getOrElse("localhost"),
+        scheme = config
+          .getStringOption("api.scheme")
+          .getOrElse("https"),
+        defaultPageSize = config
+          .getIntOption("api.pageSize")
+          .getOrElse(10),
+        pathPrefix =
+          s"${config.getStringOption("api.apiName").getOrElse("catalogue")}",
+        contextSuffix = config
+          .getStringOption("api.context.suffix")
+          .getOrElse("context.json")
+      )
+
     val router: ItemsApi = new ItemsApi {
       override implicit val ec: ExecutionContext = ecMain
       override implicit val stacksWorkService: StacksService = workService
+      override implicit val apiConfig: ApiConfig = apiConf
+
+      override def context: String = contextUri.toString
     }
 
     val appName = "ItemsApi"
