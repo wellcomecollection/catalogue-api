fc7e680b5b2ee621b12579bc3d9cba97a2322b84
jamie <j.parkinson@wellcome.ac.uk>
2021-05-05 11:13:55 +0100

    Use environment-dependent external hostname

--- terraform/modules/stack/static_responses.tf
+++ terraform/modules/stack/static_responses.tf
@@ -33,3 +33,20 @@ module "v1_gone" {
   status_code = 410
   body        = local.v1_gone_body
 }
+
+resource "aws_api_gateway_gateway_response" "not_found_404" {
+  rest_api_id   = aws_api_gateway_rest_api.catalogue.id
+  response_type = "RESOURCE_NOT_FOUND"
+  status_code   = "404"
+
+  response_templates = {
+    "application/json" = jsonencode({
+      errorType   = "http",
+      httpStatus  = 404,
+      label       = "Not Found",
+      description = "Page not found for URL $context.path"
+      type        = "Error",
+      "@context"  = "https://api.wellcomecollection.org/catalogue/v2/context.json"
+    })
+  }
+}
