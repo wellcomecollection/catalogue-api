b944ff3fc1cd44c9700b1b3ad40bd4195008a420
jamie <j.parkinson@wellcome.ac.uk>
2021-05-21 15:08:41 +0100

    Remove 'delta' suffix from staging API internal domain name

--- terraform/modules/stack/locals.tf
+++ terraform/modules/stack/locals.tf
@@ -3,7 +3,7 @@ locals {
   items_lb_port     = 8001
   version_path_part = "v2"
 
-  api_gateway_domain_name = var.environment_name != "stage" ? "catalogue.api-${var.environment_name}.wellcomecollection.org" : "catalogue.api-${var.environment_name}-delta.wellcomecollection.org"
+  api_gateway_domain_name = "catalogue.api-${var.environment_name}.wellcomecollection.org"
 
   n_private_subnets        = length(var.private_subnets)
   min_task_count           = min(var.desired_task_counts.search, var.desired_task_counts.items)
