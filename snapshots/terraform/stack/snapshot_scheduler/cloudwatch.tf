fccde4373e6e3b85ac93420e24293cba20daa74e
Robert Kenny <kenoir@gmail.com>
2021-05-27 11:50:47 +0100

    update comment on  snapshot cron

--- snapshots/terraform/stack/snapshot_scheduler/cloudwatch.tf
+++ snapshots/terraform/stack/snapshot_scheduler/cloudwatch.tf
@@ -2,6 +2,6 @@ resource "aws_cloudwatch_event_rule" "snapshot_scheduler_rule" {
   name        = "snapshot_scheduler_rule-${var.deployment_service_env}"
   description = "Starts the snapshot_scheduler (${var.deployment_service_env}) lambda"
 
-  # At 11pm every day
+  # At 11pm every day to reduce the likelihood of overlapping with a reindex.
   schedule_expression = "cron(23 0 * * ? *)"
 }
