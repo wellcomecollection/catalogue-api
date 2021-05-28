23af1608e6f999bbb5635be2106140fc34910462
Robert Kenny <kenoir@gmail.com>
2021-05-28 15:34:39 +0100

    add a comment about credentials

--- terraform/catalogue_api/locals.tf
+++ terraform/catalogue_api/locals.tf
@@ -30,6 +30,9 @@ locals {
     es_password = "catalogue/search/es_password"
   }
 
+  // TODO: Requests & Items APIs have different security profile
+  // TODO: Requests will access PII - and must have a different set of credentials!
+  // See: https://github.com/wellcomecollection/catalogue-api/issues/95
   sierra_secret_config = {
     sierra_api_key    = "stacks/prod/sierra_api_key"
     sierra_api_secret = "stacks/prod/sierra_api_secret"
