f41b0514ec33e8221b62a29bae1baebca060c202
harrisonpim <harrison.stroma@gmail.com>
2021-06-24 16:13:17 +0100

    add an archive/description recall test, document more tests with descriptions

--- rank/data/tests/works.ts
+++ rank/data/tests/works.ts
@@ -10,18 +10,27 @@ const tests: Test[] = [
     description: 'TBD',
     pass: equalTo1,
     cases: [
-      { query: 'Cassils Time lapse', ratings: ['ftqy78zj'] },
-      { query: 'stim', ratings: ['e8qxq5mv'] },
+      {
+        query: 'Cassils Time lapse',
+        ratings: ['ftqy78zj'],
+        description: 'Contributor and title in the same query',
+      },
+      {
+        query: 'stim',
+        ratings: ['e8qxq5mv'],
+        description:
+          'exact match on title with lowercasing and punctuation stripping',
+      },
       {
         query: 'bulloch history of bacteriology',
         ratings: ['rkcux48q'],
-        description: 'Contributor and title',
+        description: 'Contributor and title in the same query',
       },
       {
         query: 'stimming',
         ratings: ['uuem7v9a'],
         description:
-          'ensure that we return non-typos over typos e.g. query:stimming matches:stimming > swimming',
+          'Ensure that we return non-typos over typos e.g. query:stimming matches:stimming > swimming',
       },
       {
         query: 'The Piggle',
@@ -29,15 +38,33 @@ const tests: Test[] = [
         description:
           "Example of a known title's prefix, but not the full thing",
       },
-      { query: 'Das neue Naturheilverfahren', ratings: ['execg22x'] },
+      {
+        query: 'Das neue Naturheilverfahren',
+        ratings: ['execg22x'],
+        description:
+          "Example of a known title's prefix, but not the full thing",
+      },
       { query: 'bills of mortality', ratings: ['xwtcsk93'] },
-      { query: 'L0033046', ratings: ['kmebmktz'] },
-      { query: 'kmebmktz', ratings: ['kmebmktz'] },
-      { query: 'gzv2hhgy', ratings: ['kmebmktz'] },
-      { query: 'den', ratings: ['avqn5jd8', 'vsp8ce9z'] },
+      {
+        query: 'L0033046',
+        ratings: ['kmebmktz'],
+        description: 'miro ID matching',
+      },
+      {
+        query: 'kmebmktz',
+        ratings: ['kmebmktz'],
+        description: 'work ID matching',
+      },
+      {
+        query: 'gzv2hhgy',
+        ratings: ['kmebmktz'],
+        description: 'image ID matching',
+      },
       {
         query: 'Oxford dictionary of national biography',
         ratings: ['ruedafcw'],
+        description:
+          "Example of a known title's prefix, but not the full thing",
       },
     ],
     metric: {
@@ -56,20 +83,28 @@ const tests: Test[] = [
       {
         query: 'Atherosclerosis : an introduction to atherosclerosis',
         ratings: ['bcwvtknn', 'rty8296y'],
+        description: "two works with matching titles"
       },
       {
         query: 'wa/hmm durham',
         ratings: ['euf49qkx', 'tpxy78kr', 'gu3z98y4', 'ad3rfubw'],
+        description: 'archive refno and a word from the title',
+      },
+      {
+        query: 'wa/hmm benin',
+        ratings: ['qfdvkegw', 'je5pm2gj', 'dppjjtqz'],
+        description: 'archive refno and a word from the description',
       },
       {
         query: 'eugenics society annual reports',
         ratings: ['k9w95csw', 'asqf8kzb', 'n499pzsr'],
+        description: "matches archives without providing refnos"
       },
     ],
     metric: {
       recall: {
         relevant_rating_threshold: 3,
-        k: 5,
+        k: 100,
       },
     },
   },
