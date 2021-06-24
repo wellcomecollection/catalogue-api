2e1d838c8c60423ba6ad117d1ed995238a77397b
James Gorrie <j.gorrie@wellcome.org>
2021-06-24 18:51:51 +0100

    few improvements to index2

--- rank/pages/index2.tsx
+++ rank/pages/index2.tsx
@@ -5,6 +5,7 @@ import tests from '../data/tests'
 import service, { SearchTemplate } from '../services/search-templates'
 import { Env } from '../types/env'
 import { Namespace } from '../types/namespace'
+import { Test } from '../types/test'
 import { TestResult } from './api/eval'
 
 type Props = {
@@ -21,20 +22,18 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
 }
 
 type TestRunProps = {
-  index: string
-  env: Env
-  testId: string
-  namespace: Namespace
+  test: Test
+  template: SearchTemplate
 }
-const TestRun: FC<TestRunProps> = ({ index, env, testId, namespace }) => {
+const TestRun: FC<TestRunProps> = ({ test, template }) => {
   const [testResult, setTestResult] = useState<TestResult>()
 
-  const envIndex = `${env}:${index}`
+  const envIndex = `${template.env}:${template.index}`
   useEffect(() => {
     const fetchData = async () => {
       const { origin } = absoluteUrl()
       const data: TestResult = await fetch(
-        `${origin}/api/test?index=${envIndex}&testId=${testId}&namespace=${namespace}`
+        `${origin}/api/test?index=${envIndex}&testId=${test.id}&namespace=${template.namespace}`
       ).then((res) => res.json())
 
       setTestResult(data)
@@ -44,23 +43,23 @@ const TestRun: FC<TestRunProps> = ({ index, env, testId, namespace }) => {
 
   return (
     <div>
-      {testResult && (
-        <>
-          {testResult && (
-            <div>
-              <h2 className="text-lg">{env}</h2>
-              {testResult.results.map((result, i) => (
-                <div key={`${testId}-${env}-${i}`}>
-                  <pre className="inline-block">
-                    {result.result.pass ? ' pass ' : ' fail '}
-                  </pre>
-                  | {result.query}
-                </div>
-              ))}
-            </div>
-          )}
-        </>
-      )}
+      <>
+        <div>
+          <div className="text-sm">
+            <div>env: {template.env}</div>
+            <div>index: {template.index}</div>
+          </div>
+          {testResult &&
+            testResult.results.map((result, i) => (
+              <div key={`${test.id}-${template.env}-${i}`}>
+                <pre className="inline-block">
+                  {result.result.pass ? ' pass ' : ' fail '}
+                </pre>
+                | {result.query}
+              </div>
+            ))}
+        </div>
+      </>
     </div>
   )
 }
@@ -75,15 +74,15 @@ export const Index2: NextPage<Props> = ({ searchTemplates }) => {
 
   return (
     <>
-      {worksTests.map(({ id: testId, label, description, cases }) => (
-        <div key={`works-${testId}`}>
-          <h2 className="text-xl">{label}</h2>
+      {worksTests.map((test) => (
+        <div key={`works-${test.id}`}>
+          <h2 className="text-xl">{test.label}</h2>
           <div className={`grid grid-cols-${worksTemplates.length}`}>
-            {worksTemplates.map(({ env, namespace, index }) => {
+            {worksTemplates.map((template) => {
               return (
                 <TestRun
-                  key={`works-${testId}-${env}-${index}`}
-                  {...{ testId, index, env, namespace }}
+                  key={`works-${test.id}-${template.env}-${template.index}`}
+                  {...{ test, template }}
                 />
               )
             })}
