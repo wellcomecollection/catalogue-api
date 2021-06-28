abb419b0c46b6847dbb0eb591c3d4708f90b6aeb
Alex Chan <a.chan@wellcome.org>
2021-06-28 11:56:01 +0100

    Bump to the latest index name

--- common/display/src/main/scala/weco/catalogue/display_model/ElasticConfig.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/ElasticConfig.scala
@@ -12,7 +12,7 @@ case class ElasticConfig(
 object ElasticConfig {
   // We use this to share config across API applications
   // i.e. The API and the snapshot generator.
-  val indexDate = "2021-06-08"
+  val indexDate = "2021-06-28"
 
   def apply(): ElasticConfig =
     ElasticConfig(
