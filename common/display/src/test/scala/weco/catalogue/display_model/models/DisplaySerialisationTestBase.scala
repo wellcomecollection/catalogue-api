45a93a6c800108330df1c5256f040b5026b11ddd
James Gorrie <j.gorrie@wellcome.org>
2021-07-07 16:38:41 +0100

    add trailingComma = false

--- common/display/src/test/scala/weco/catalogue/display_model/models/DisplaySerialisationTestBase.scala
+++ common/display/src/test/scala/weco/catalogue/display_model/models/DisplaySerialisationTestBase.scala
@@ -100,7 +100,7 @@ trait DisplaySerialisationTestBase {
           "id": "${DisplayAccessMethod(cond.method).id}",
           "label": "${DisplayAccessMethod(cond.method).label}"
         },
-        ${optionalString("terms", cond.terms)}
+        ${optionalString("terms", cond.terms, trailingComma = false)}
         ${optionalObject("status", accessStatus, cond.status)}
       }
     """
