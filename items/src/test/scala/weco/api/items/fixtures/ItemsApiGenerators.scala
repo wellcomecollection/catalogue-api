be5ed7db4f200ee120b42b9850fb9e5441fa5561
Robert Kenny <kenoir@gmail.com>
2021-07-20 15:27:17 +0100

    use new mthods on createPhysicalLocationWith, add mechanism to generate single entry

--- items/src/test/scala/weco/api/items/fixtures/ItemsApiGenerators.scala
+++ items/src/test/scala/weco/api/items/fixtures/ItemsApiGenerators.scala
@@ -1,24 +1,12 @@
 package weco.api.items.fixtures
 
-import akka.http.scaladsl.model.{
-  ContentTypes,
-  HttpEntity,
-  HttpRequest,
-  HttpResponse
-}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpRequest, HttpResponse}
 import org.scalatest.Suite
 import weco.api.items.services.ItemUpdateService
 import weco.catalogue.internal_model.identifiers.IdState
-import weco.catalogue.internal_model.locations.{
-  AccessCondition,
-  LocationType,
-  PhysicalLocation
-}
+import weco.catalogue.internal_model.locations.{AccessCondition, LocationType, PhysicalLocation}
 import weco.catalogue.internal_model.work.Item
-import weco.catalogue.internal_model.work.generators.{
-  ItemsGenerators,
-  WorkGenerators
-}
+import weco.catalogue.internal_model.work.generators.{ItemsGenerators, WorkGenerators}
 import weco.catalogue.source_model.sierra.identifiers.SierraItemNumber
 import weco.fixtures.TestWith
 
@@ -39,34 +27,49 @@ trait ItemsApiGenerators
       }
     }
 
+  def buildEntry(
+                  sierraItemNumber: SierraItemNumber,
+                  deleted: String = "false",
+                  suppressed: String = "false",
+                  holdCount: Int = 0
+                ) =  f"""
+                        |{
+                        |  "id": "${sierraItemNumber.withCheckDigit}",
+                        |  "deleted": ${deleted},
+                        |  "suppressed": ${suppressed},
+                        |  "fixedFields": {
+                        |    "79": {"label": "LOCATION", "value": "scmwf", "display": "Closed stores A&MSS Well.Found."},
+                        |    "88": {"label": "STATUS", "value": "-", "display": "Available"},
+                        |    "108": {"label": "OPACMSG", "value": "f", "display": "Online request"}
+                        |  },
+                        |  "holdCount": ${holdCount}
+                        |}
+                        |""".stripMargin
+
   def sierraItemResponse(
     sierraItemNumber: SierraItemNumber,
     deleted: String = "false",
     suppressed: String = "false",
     holdCount: Int = 0
-  ): HttpEntity.Strict =
+  ): HttpEntity.Strict = {
+
+    val entries = Seq(
+      buildEntry(sierraItemNumber, deleted, suppressed, holdCount)
+    ).mkString(",\n")
+
     HttpEntity(
       contentType = ContentTypes.`application/json`,
       f"""
-      |{
-      |  "total": 1,
-      |  "start": 0,
-      |  "entries": [
-      |    {
-      |      "id": "${sierraItemNumber.withCheckDigit}",
-      |      "deleted": ${deleted},
-      |      "suppressed": ${suppressed},
-      |      "fixedFields": {
-      |        "79": {"label": "LOCATION", "value": "scmwf", "display": "Closed stores A&MSS Well.Found."},
-      |        "88": {"label": "STATUS", "value": "-", "display": "Available"},
-      |        "108": {"label": "OPACMSG", "value": "f", "display": "Online request"}
-      |      },
-      |      "holdCount": ${holdCount}
-      |    }
-      |  ]
-      |}
-      |""".stripMargin
+        |{
+        |  "total": ${entries.size},
+        |  "start": 0,
+        |  "entries": [
+        |    $entries
+        |  ]
+        |}
+        |""".stripMargin
     )
+  }
 
   def createPhysicalItemWith(
     sierraItemNumber: SierraItemNumber,
@@ -75,9 +78,10 @@ trait ItemsApiGenerators
 
     val physicalItemLocation: PhysicalLocation = createPhysicalLocationWith(
       accessConditions = List(accessCondition),
-      locationType = LocationType.ClosedStores
-      // TODO: Update this to use methods on createPhysicalLocationWith
-    ).copy(license = None, shelfmark = None)
+      locationType = LocationType.ClosedStores,
+      license = None,
+      shelfmark = None
+    )
 
     val itemSourceIdentifier = createSierraSystemSourceIdentifierWith(
       value = sierraItemNumber.withCheckDigit,
