68c3904570d25bccf32f5cdf11dc72601483826b
Robert Kenny <kenoir@gmail.com>
2021-07-22 15:39:10 +0100

    Add some more tests for sierrasource

--- common/stacks/src/test/scala/weco/api/stacks/services/SierraServiceTest.scala
+++ common/stacks/src/test/scala/weco/api/stacks/services/SierraServiceTest.scala
@@ -82,11 +82,124 @@ class SierraServiceTest
           val future = service.getAccessConditions(Seq(itemNumber))
 
           whenReady(future) {
-            _ shouldBe Map(
-              itemNumber -> Some(
-                AccessCondition(method = AccessMethod.OnlineRequest)
+            _ shouldBe Map(itemNumber ->
+              AccessCondition(method = AccessMethod.OnlineRequest)
+            )
+          }
+        }
+      }
+
+      it("if some entries are missing, returns the available AccessConditions") {
+        val responses = Seq(
+          (
+            HttpRequest(
+              uri = Uri(
+                "http://sierra:1234/v5/items?id=1601017,1234567&fields=deleted,fixedFields,holdCount,suppressed"
+              )
+            ),
+            HttpResponse(
+              entity = HttpEntity(
+                contentType = ContentTypes.`application/json`,
+                """
+                  |{
+                  |  "total": 1,
+                  |  "start": 0,
+                  |  "entries": [
+                  |    {
+                  |      "id": "1601017",
+                  |      "deleted": false,
+                  |      "suppressed": false,
+                  |      "fixedFields": {
+                  |        "79": {"label": "LOCATION", "value": "scmwf", "display": "Closed stores A&MSS Well.Found."},
+                  |        "88": {"label": "STATUS", "value": "-", "display": "Available"},
+                  |        "108": {"label": "OPACMSG", "value": "f", "display": "Online request"}
+                  |      },
+                  |      "holdCount": 0
+                  |    }
+                  |  ]
+                  |}
+                  |""".stripMargin
               )
             )
+          )
+        )
+
+        withMaterializer { implicit mat =>
+          val service = SierraService(
+            client = new MemoryHttpClient(responses) with HttpGet
+              with HttpPost {
+              override val baseUri: Uri = Uri("http://sierra:1234")
+            }
+          )
+
+          val itemNumber = SierraItemIdentifier.fromSourceIdentifier(
+            SourceIdentifier(
+              identifierType = SierraSystemNumber,
+              value = "i16010176",
+              ontologyType = "Item"
+            ))
+
+          val missingItemNumber = SierraItemIdentifier.fromSourceIdentifier(
+            SourceIdentifier(
+              identifierType = SierraSystemNumber,
+              value = "i12345672",
+              ontologyType = "Item"
+            ))
+
+          val future = service.getAccessConditions(Seq(itemNumber, missingItemNumber))
+
+          whenReady(future) {
+            _ shouldBe Map(itemNumber ->
+              AccessCondition(method = AccessMethod.OnlineRequest)
+            )
+          }
+        }
+      }
+
+      it("if all entries are missing, returns no AccessConditions") {
+        val responses = Seq(
+          (
+            HttpRequest(
+              uri = Uri(
+                "http://sierra:1234/v5/items?id=1601017&fields=deleted,fixedFields,holdCount,suppressed"
+              )
+            ),
+            HttpResponse(
+              status = StatusCodes.NotFound,
+              entity = HttpEntity(
+                contentType = ContentTypes.`application/json`,
+                """
+                  |{
+                  |  "code": 107,
+                  |  "specificCode": 0,
+                  |  "httpStatus": 404,
+                  |  "name": "Record not found"
+                  |}
+                  |""".stripMargin
+              )
+            )
+          )
+        )
+
+        withMaterializer { implicit mat =>
+          val service = SierraService(
+            client = new MemoryHttpClient(responses) with HttpGet
+              with HttpPost {
+              override val baseUri: Uri = Uri("http://sierra:1234")
+            }
+          )
+
+          val itemNumber = SierraItemIdentifier.fromSourceIdentifier(
+            SourceIdentifier(
+              identifierType = SierraSystemNumber,
+              value = "i16010176",
+              ontologyType = "Item"
+            ))
+
+          val future = service.getAccessConditions(Seq(itemNumber))
+
+          whenReady(future) {
+            _ shouldBe Map.empty
           }
         }
       }
