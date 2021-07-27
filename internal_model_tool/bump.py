44ba6eff3207fd52c15518be2ad76a8b4b1687c4
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-07-27 13:13:05 +0000

    Apply auto-formatting rules

--- internal_model_tool/bump.py
+++ internal_model_tool/bump.py
@@ -6,11 +6,18 @@ to the latest version in in the catalogue-api `mappings._meta`.
 This saves somebody having to look up what the exact version/commit ID is.
 """
 
-from common import get_session, get_local_date, get_remote_latest_internal_model, set_local_internal_model
+from common import (
+    get_session,
+    get_local_date,
+    get_remote_latest_internal_model,
+    set_local_internal_model,
+)
 
 if __name__ == "__main__":
     session = get_session(role_arn="arn:aws:iam::760097843905:role/platform-read_only")
-    catalogue_session = get_session(role_arn="arn:aws:iam::756629837203:role/catalogue-developer")
+    catalogue_session = get_session(
+        role_arn="arn:aws:iam::756629837203:role/catalogue-developer"
+    )
     date = get_local_date()
     latest_internal_model = get_remote_latest_internal_model(catalogue_session, date)
     set_local_internal_model(latest_internal_model)
--- internal_model_tool/check_compatibility.py
+++ internal_model_tool/check_compatibility.py
@@ -4,19 +4,26 @@ This script checks the local version against what's been set
 as the latest version in in the catalogue-api `mappings._meta`.pwd
 """
 
-from common import get_session, get_local_date, get_local_internal_model, get_remote_meta
+from common import (
+    get_session,
+    get_local_date,
+    get_local_internal_model,
+    get_remote_meta,
+)
 
 
 if __name__ == "__main__":
     session = get_session(role_arn="arn:aws:iam::760097843905:role/platform-read_only")
-    catalogue_session = get_session(role_arn="arn:aws:iam::756629837203:role/catalogue-developer")
+    catalogue_session = get_session(
+        role_arn="arn:aws:iam::756629837203:role/catalogue-developer"
+    )
     date = get_local_date()
     internal_model = get_local_internal_model()
     version, hash = internal_model.split(".")
     meta = get_remote_meta(catalogue_session, date)
     meta_key = f"model.versions.{version}"
     is_compatible = meta_key in meta and meta[meta_key] == hash
-    if (is_compatible):
+    if is_compatible:
         print(f"{meta_key} is compatible with {meta}")
         exit(0)
     else:
