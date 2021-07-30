f7a14b7eee41401544dc09c2b8d42c3f54811038
Alex Chan <a.chan@wellcome.org>
2021-07-30 12:44:44 +0100

    Remove two now-unused ECR repositories

--- terraform/shared/ecr.tf
+++ terraform/shared/ecr.tf
@@ -1,13 +1,3 @@
-# DEPRECATED! delete after switching to items
-resource "aws_ecr_repository" "items_api" {
-  name = "uk.ac.wellcome/items_api"
-}
-
-# DEPRECATED! delete after switching to requests
-resource "aws_ecr_repository" "requests_api" {
-  name = "uk.ac.wellcome/requests_api"
-}
-
 resource "aws_ecr_repository" "items" {
   name = "uk.ac.wellcome/items"
 
