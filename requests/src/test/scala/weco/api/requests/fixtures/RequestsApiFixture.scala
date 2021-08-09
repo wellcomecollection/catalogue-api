8e37f052c9eb0537bc665866035a12379a17c365
Alex Chan <a.chan@wellcome.org>
2021-08-09 11:09:51 +0100

    Use the shared fixture in the requests API tests

--- requests/src/test/scala/weco/api/requests/fixtures/RequestsApiFixture.scala
+++ requests/src/test/scala/weco/api/requests/fixtures/RequestsApiFixture.scala
@@ -1,16 +1,15 @@
 package weco.api.requests.fixtures
 
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
-import weco.fixtures.TestWith
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import weco.api.requests.RequestsApi
 import weco.api.search.models.ApiConfig
-import weco.api.stacks.services.{ItemLookup, SierraService}
-import weco.http.client.{HttpGet, HttpPost, MemoryHttpClient}
-import weco.http.fixtures.HttpFixtures
+import weco.api.stacks.fixtures.SierraServiceFixture
+import weco.api.stacks.services.ItemLookup
+import weco.fixtures.TestWith
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait RequestsApiFixture extends HttpFixtures {
+trait RequestsApiFixture extends SierraServiceFixture {
 
   val metricsName = "RequestsApiFixture"
 
@@ -27,19 +26,15 @@ trait RequestsApiFixture extends HttpFixtures {
     responses: Seq[(HttpRequest, HttpResponse)] = Seq()
   )(testWith: TestWith[Unit, R]): R =
     withMaterializer { implicit mat =>
-      val sierraService = SierraService(
-        new MemoryHttpClient(responses) with HttpGet with HttpPost {
-          override val baseUri: Uri = Uri("http://sierra:1234")
+      withSierraService(responses) { sierraService =>
+        val api: RequestsApi = new RequestsApi(
+          sierraService = sierraService,
+          itemLookup = itemLookup
+        )
+
+        withApp(api.routes) { _ =>
+          testWith(())
         }
-      )
-
-      val api: RequestsApi = new RequestsApi(
-        sierraService = sierraService,
-        itemLookup = itemLookup
-      )
-
-      withApp(api.routes) { _ =>
-        testWith(())
       }
     }
 }
