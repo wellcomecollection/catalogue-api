26eba5bf8d30ab8724bee2e56eaefad88fe26c26
Alex Chan <a.chan@wellcome.org>
2021-08-09 16:10:46 +0100

    Bump the index date in ElasticConfig

--- common/display/src/main/scala/weco/catalogue/display_model/ElasticConfig.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/ElasticConfig.scala
@@ -12,7 +12,7 @@ case class ElasticConfig(
 object ElasticConfig {
   // We use this to share config across API applications
   // i.e. The API and the snapshot generator.
-  val indexDate = "2021-07-19"
+  val indexDate = "2021-08-09"
 
   def apply(): ElasticConfig =
     ElasticConfig(
