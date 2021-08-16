66c6ea9f69e62633b6e47993d8176fc627e99c6d
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-08-16 14:55:01 +0000

    Apply auto-formatting rules

--- common/search/src/main/scala/weco/api/search/elasticsearch/ElasticsearchService.scala
+++ common/search/src/main/scala/weco/api/search/elasticsearch/ElasticsearchService.scala
@@ -3,7 +3,12 @@ package weco.api.search.elasticsearch
 import com.sksamuel.elastic4s.ElasticDsl._
 import com.sksamuel.elastic4s.circe._
 import com.sksamuel.elastic4s.requests.get.GetResponse
-import com.sksamuel.elastic4s.requests.searches.{MultiSearchRequest, MultiSearchResponse, SearchRequest, SearchResponse}
+import com.sksamuel.elastic4s.requests.searches.{
+  MultiSearchRequest,
+  MultiSearchResponse,
+  SearchRequest,
+  SearchResponse
+}
 import com.sksamuel.elastic4s.{ElasticClient, Hit, Index, Response}
 import grizzled.slf4j.Logging
 import io.circe.Decoder
@@ -73,8 +78,8 @@ class ElasticsearchService(elasticClient: ElasticClient)(
     }
 
   def executeMultiSearchRequest(
-                            request: MultiSearchRequest
-                          ): Future[Either[ElasticsearchError, MultiSearchResponse]] =
+    request: MultiSearchRequest
+  ): Future[Either[ElasticsearchError, MultiSearchResponse]] =
     spanFuture(
       name = "ElasticSearch#executeMultiSearchRequest",
       spanType = "request",
@@ -87,11 +92,12 @@ class ElasticsearchService(elasticClient: ElasticClient)(
         .map(_.toEither)
         .map {
           case Right(multiResponse) =>
-            val accumulatedTime = multiResponse.items.foldLeft(0L) { (acc, item) =>
-              item.response match {
-                case Right(itemResponse) => acc + itemResponse.took
-                case Left(err) => acc
-              }
+            val accumulatedTime = multiResponse.items.foldLeft(0L) {
+              (acc, item) =>
+                item.response match {
+                  case Right(itemResponse) => acc + itemResponse.took
+                  case Left(err)           => acc
+                }
             }
 
             transaction.setLabel("elasticTook", accumulatedTime)
