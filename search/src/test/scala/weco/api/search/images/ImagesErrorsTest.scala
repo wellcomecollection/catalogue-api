cefd368d503804043b626ae0c084d9ba6169376b
Alex Chan <a.chan@wellcome.org>
2021-09-29 10:16:05 +0100

    Add some tests that we 500 if the default index is missing

--- search/src/test/scala/weco/api/search/images/ImagesErrorsTest.scala
+++ search/src/test/scala/weco/api/search/images/ImagesErrorsTest.scala
@@ -1,8 +1,10 @@
 package weco.api.search.images
 
 import org.scalatest.Assertion
+import org.scalatest.prop.TableDrivenPropertyChecks
+import weco.catalogue.display_model.ElasticConfig
 
-class ImagesErrorsTest extends ApiImagesTestBase {
+class ImagesErrorsTest extends ApiImagesTestBase with TableDrivenPropertyChecks {
   describe("returns a 404 for missing resources") {
     it("looking up an image that doesn't exist") {
       val id = "blahblah"
@@ -10,6 +12,53 @@ class ImagesErrorsTest extends ApiImagesTestBase {
         s"$rootPath/images/$id",
         description = s"Image not found for identifier $id"
       )
+    }
+  }
+
+  it("returns a 404 error if the user asks for a non-existent index") {
+    withImagesApi { case (_, routes) =>
+      val indexName = createIndexName
+
+      val testPaths = Table(
+        "path",
+        s"$rootPath/images?_index=$indexName",
+        s"$rootPath/images?_index=$indexName&query=fish",
+        s"$rootPath/images/$createCanonicalId?_index=$indexName"
+      )
+
+      forAll(testPaths) { path =>
+        assertIsNotFound(path, description = s"There is no index $indexName")
+      }
+    }
+  }
+
+  it("returns a 500 error if the default index doesn't exist") {
+    val elasticConfig = ElasticConfig(
+      worksIndex = createIndex,
+      imagesIndex = createIndex
+    )
+
+    val testPaths = Table(
+      "path",
+      s"$rootPath/images",
+      s"$rootPath/images?query=fish",
+      s"$rootPath/images/$createCanonicalId"
+    )
+
+    withRouter(elasticConfig) { routes =>
+      forAll(testPaths) { path =>
+        assertJsonResponse(routes, path)(
+          Status.InternalServerError ->
+            s"""
+               |{
+               |  "type": "Error",
+               |  "errorType": "http",
+               |  "httpStatus": 500,
+               |  "label": "Internal Server Error"
+               |}
+            """.stripMargin
+        )
+      }
     }
   }
 
