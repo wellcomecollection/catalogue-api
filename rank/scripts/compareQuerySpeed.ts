00be066b161502ced601357f1dad8e6131873b23
harrisonpim <harrison.stroma@gmail.com>
2021-10-28 17:33:13 +0100

    add a script to compare response times for candidate and prod queries

---
+++ rank/scripts/compareQuerySpeed.ts
@@ -0,0 +1,77 @@
+import {
+  QueryEnv,
+  SearchTemplate,
+  namespaces,
+  queryEnvs,
+} from '../types/searchTemplate'
+import { getQueries, listIndices } from '../services/search-templates'
+import { histogram, info } from './utils'
+
+import bars from 'bars'
+import { getRankClient } from '../services/elasticsearch'
+import prompts from 'prompts'
+
+global.fetch = require('node-fetch')
+
+async function go() {
+  const queries = await getQueries()
+  const indices = await listIndices()
+
+  const namespace: string = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Which namespace do you want to test in?',
+    choices: namespaces.map((namespace) => ({
+      title: namespace,
+      value: namespace,
+    })),
+  }).then(({ value }) => value)
+  const testIndices = indices.filter((index) => index.startsWith(namespace))
+  info(
+    `Found ${testIndices.length} indices for testing: ${testIndices.toString()}`
+  )
+
+  let searchTemplates: SearchTemplate[] = []
+  for (const index of testIndices) {
+    const queryEnv: QueryEnv = await prompts({
+      type: 'select',
+      name: 'value',
+      message: `Which query should be used with ${index}?`,
+      choices: queryEnvs.map((env) => ({
+        title: env,
+        value: env,
+      })),
+    }).then(({ value }) => value)
+    const query = queries[queryEnv][namespace]
+    searchTemplates.push(new SearchTemplate(queryEnv, index, query))
+  }
+
+  info('Loading real search terms for querying')
+  const { terms } = await import(`../data/terms/${namespace}.json`).then(
+    (mod) => mod.default
+  )
+
+  // to minimise the effect of network issues, caching etc, between candidates,
+  // we use the multisearch API and make concurrent requests to multiple indices.
+  info('Running queries\n')
+  const multiGetTimes: [number, number][] = await Promise.all(
+    terms.map(async (term) => {
+      const body = searchTemplates.flatMap(({ index, query }) => [
+        { index },
+        { query },
+      ])
+      const client = await getRankClient()
+      const { body: searchResp } = await client.msearch({ body })
+      return searchResp.responses.map((r) => r.took)
+    })
+  )
+
+  testIndices.map((index, i) => {
+    const times = multiGetTimes.map((x) => x[i])
+    info(index)
+    info(`mean: ${times.reduce((a, b) => a + b, 0) / times.length}`)
+    console.log(bars(histogram(times)))
+  })
+}
+
+go()
--- rank/scripts/getSearchTerms.ts
+++ rank/scripts/getSearchTerms.ts
@@ -1,6 +1,5 @@
-import { code, info, p, pretty, success } from './utils'
+import { p, pretty, success } from './utils'
 
-import { SearchResponse } from '../types/elasticsearch'
 import fs from 'fs'
 import { getReportingClient } from '../services/elasticsearch'
 import { namespaces } from '../types/searchTemplate'
@@ -9,9 +8,6 @@ import prompts from 'prompts'
 global.fetch = require('node-fetch')
 
 async function go() {
-  // get some real queries from the reporting index
-  //   nb get unique ones using a terms aggregation
-  //   https://stackoverflow.com/questions/25465215/elasticsearch-return-unique-values
   const chosenNamespaces: string[] = await prompts({
     type: 'multiselect',
     name: 'value',
@@ -23,39 +19,36 @@ async function go() {
   }).then(({ value }) => value)
 
   chosenNamespaces.map(async (namespace) => {
-    const searchResp = await getReportingClient()
-      .search({
-        index: 'metrics-conversion-prod',
-        body: {
-          size: 5000,
-          _source: ['page.query.query'],
-          query: {
-            bool: {
-              must: [
-                {
-                  exists: {
-                    field: 'page.query.query',
-                  },
+    const { body: searchResp } = await getReportingClient().search({
+      index: 'metrics-conversion-prod',
+      body: {
+        size: 5000,
+        _source: ['page.query.query'],
+        query: {
+          bool: {
+            must: [
+              {
+                exists: {
+                  field: 'page.query.query',
                 },
-                {
-                  match: {
-                    'page.name': namespace,
-                  },
+              },
+              {
+                match: {
+                  'page.name': namespace,
                 },
-              ],
-            },
+              },
+            ],
           },
         },
-      })
-      .then((res) => res.body)
+      },
+    })
     success(`Fetched recent search terms for ${namespace}`)
 
-    const terms = {
-      terms: searchResp.hits.hits
-        .map((hit) => hit._source.page.query.query)
-        .filter((item, i, ar) => ar.indexOf(item) === i),
-    }
-    fs.writeFileSync(p([`../data/terms/${namespace}.json`]), pretty(terms))
+    const terms = searchResp.hits.hits
+      .map((hit) => hit._source.page.query.query)
+      .filter((term, i, ar) => ar.indexOf(term) === i) // keep unique
+
+    fs.writeFileSync(p([`../data/terms/${namespace}.json`]), pretty({ terms }))
     success(`Wrote terms to ./data/terms/${namespace}.json\n`)
   })
 }
