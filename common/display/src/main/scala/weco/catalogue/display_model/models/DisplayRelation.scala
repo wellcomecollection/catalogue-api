3d69ffa34ac5b9abb19120a955f1d2826dba12f3
Paul Butcher <paul.butcher+github@gmail.com>
2022-02-23 16:41:59 +0000

    Revert "Relation.id is now optional and hierarchies can include Series"
    This reverts commit 3cd554dc7f990404e1394d8140fda386121f7566.

--- common/display/src/main/scala/weco/catalogue/display_model/models/DisplayRelation.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/models/DisplayRelation.scala
@@ -9,7 +9,7 @@ case class DisplayRelation(
   @Schema(
     accessMode = Schema.AccessMode.READ_ONLY,
     description = "The canonical identifier given to a thing."
-  ) id: Option[String],
+  ) id: String,
   @Schema(
     description =
       "The title or other short label of a work, including labels not present in the actual work or item but applied by the cataloguer for the purposes of search or description."
@@ -38,7 +38,7 @@ object DisplayRelation {
 
   def apply(relation: Relation): DisplayRelation =
     DisplayRelation(
-      id = relation.id.map { _.underlying },
+      id = relation.id.underlying,
       title = relation.title,
       referenceNumber = relation.collectionPath.flatMap(_.label),
       ontologyType = DisplayWork.displayWorkType(relation.workType),
