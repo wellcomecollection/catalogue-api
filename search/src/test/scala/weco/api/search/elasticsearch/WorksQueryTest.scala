cbdafb628a574208956e7f90a19bef314276f77c
Alex Chan <alex@alexwlchan.net>
2022-04-29 09:16:47 +0100

    Get WorksIncludesTest working

--- search/src/test/scala/weco/api/search/elasticsearch/WorksQueryTest.scala
+++ search/src/test/scala/weco/api/search/elasticsearch/WorksQueryTest.scala
@@ -6,11 +6,7 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.{Assertion, EitherValues}
 import weco.catalogue.internal_model.Implicits._
 import weco.catalogue.internal_model.index.IndexFixtures
-import weco.catalogue.internal_model.work.generators.{
-  ContributorGenerators,
-  GenreGenerators,
-  SubjectGenerators
-}
+import weco.catalogue.internal_model.work.generators.{ContributorGenerators, GenreGenerators, SubjectGenerators}
 import weco.api.search.generators.SearchOptionsGenerators
 import weco.api.search.models.{SearchQuery, SearchQueryType, WorkSearchOptions}
 import weco.api.search.services.{IndexedWork, WorksService}
