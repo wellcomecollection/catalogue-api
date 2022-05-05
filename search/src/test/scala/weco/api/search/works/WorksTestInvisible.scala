816bd965fd74efe33a7074e9e6db0cdda259ed63
Alex Chan <alex@alexwlchan.net>
2022-05-05 13:50:39 +0100

    Remove some unused imports

--- search/src/test/scala/weco/api/search/works/WorksTestInvisible.scala
+++ search/src/test/scala/weco/api/search/works/WorksTestInvisible.scala
@@ -1,9 +1,6 @@
 package weco.api.search.works
 
 import weco.api.search.fixtures.TestDocumentFixtures
-import weco.catalogue.internal_model.Implicits._
-import weco.catalogue.internal_model.work.Work
-import weco.catalogue.internal_model.work.WorkState.Indexed
 
 class WorksTestInvisible extends ApiWorksTestBase with TestDocumentFixtures {
   it("returns an HTTP 410 Gone if looking up a work with visible = false") {
