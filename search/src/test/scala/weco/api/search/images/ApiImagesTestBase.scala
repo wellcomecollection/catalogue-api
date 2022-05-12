7f0820f87225e142f9b92b049dee2a15d68d5636
Alex Chan <alex@alexwlchan.net>
2022-05-12 12:58:10 +0100

    Remove more pieces from ApiImagesTestBase

--- search/src/test/scala/weco/api/search/images/ApiImagesTestBase.scala
+++ search/src/test/scala/weco/api/search/images/ApiImagesTestBase.scala
@@ -4,44 +4,11 @@ import weco.api.search.ApiTestBase
 import weco.api.search.fixtures.TestDocumentFixtures
 import weco.api.search.json.CatalogueJsonUtil
 import weco.api.search.models.request.SingleImageIncludes
-import weco.catalogue.display_model.test.util.DisplaySerialisationTestBase
-import weco.catalogue.internal_model.generators.ImageGenerators
-import weco.catalogue.internal_model.image._
 
 trait ApiImagesTestBase
     extends ApiTestBase
-    with DisplaySerialisationTestBase
-    with ImageGenerators
     with CatalogueJsonUtil
     with TestDocumentFixtures {
-
-  def singleImageResult: String =
-    s"""
-       |  "type": "Image"
-     """.stripMargin
-
-  def imageSource(source: ImageSource): String =
-    source match {
-      case ParentWorks(work, _) =>
-        s"""
-           |{
-           |  "id": "${source.id.canonicalId}",
-           |  "title": ${work.data.title.map(_.toJson)},
-           |  "type": "Work"
-           |}
-           |""".stripMargin.tidy
-    }
-
-  def imageResponse(image: Image[ImageState.Indexed]): String =
-    s"""
-       |  {
-       |    "type": "Image",
-       |    "id": "${image.id}",
-       |    "thumbnail": ${location(image.state.derivedData.thumbnail)},
-       |    "locations": [${locations(image.locations)}],
-       |    "source": ${imageSource(image.source)}
-       |  }
-     """.stripMargin
 
   def imagesListResponse(
     ids: Seq[String],
