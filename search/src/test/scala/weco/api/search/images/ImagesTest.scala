68b63dc721af0ab8932f5ac522ee19c4dc1f07ab
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-05-12 10:37:22 +0000

    Apply auto-formatting rules

--- search/src/test/scala/weco/api/search/images/ImagesTest.scala
+++ search/src/test/scala/weco/api/search/images/ImagesTest.scala
@@ -7,11 +7,14 @@ class ImagesTest extends ApiImagesTestBase {
     withImagesApi {
       case (imagesIndex, routes) =>
         indexTestImages(
-          imagesIndex, (0 to 6).map(i => s"images.different-licenses.$i"): _*
+          imagesIndex,
+          (0 to 6).map(i => s"images.different-licenses.$i"): _*
         )
 
         assertJsonResponse(routes, path = s"$rootPath/images") {
-          Status.OK -> newImagesListResponse(ids = (0 to 6).map(i => s"images.different-licenses.$i"))
+          Status.OK -> newImagesListResponse(
+            ids = (0 to 6).map(i => s"images.different-licenses.$i")
+          )
         }
     }
   }
@@ -20,7 +23,8 @@ class ImagesTest extends ApiImagesTestBase {
     withImagesApi {
       case (imagesIndex, routes) =>
         indexTestImages(
-          imagesIndex, (0 to 6).map(i => s"images.different-licenses.$i"): _*
+          imagesIndex,
+          (0 to 6).map(i => s"images.different-licenses.$i"): _*
         )
 
         assertJsonResponse(routes, path = s"$rootPath/images/5vamzxd8") {
@@ -33,12 +37,14 @@ class ImagesTest extends ApiImagesTestBase {
   }
 
   it("returns only linked images when a source work ID is requested") {
-    val workImages = (0 to 3).map(i => s"images.examples.linked-with-the-same-work.$i")
+    val workImages =
+      (0 to 3).map(i => s"images.examples.linked-with-the-same-work.$i")
 
     withImagesApi {
       case (imagesIndex, routes) =>
         indexTestImages(
-          imagesIndex, workImages :+ "images.examples.linked-with-another-work": _*,
+          imagesIndex,
+          workImages :+ "images.examples.linked-with-another-work": _*
         )
 
         assertJsonResponse(
@@ -62,8 +68,6 @@ class ImagesTest extends ApiImagesTestBase {
     }
   }
 
-
-
   it("returns matching results when using work data") {
     withImagesApi {
       case (imagesIndex, routes) =>
@@ -71,19 +75,23 @@ class ImagesTest extends ApiImagesTestBase {
           imagesIndex,
           "images.examples.bread-baguette",
           "images.examples.bread-focaccia",
-          "images.examples.bread-mantou",
+          "images.examples.bread-mantou"
         )
 
         assertJsonResponse(routes, path = s"$rootPath/images?query=bread") {
           Status.OK -> newImagesListResponse(
-            ids = List("images.examples.bread-baguette",
+            ids = List(
+              "images.examples.bread-baguette",
               "images.examples.bread-focaccia",
-              "images.examples.bread-mantou"),
+              "images.examples.bread-mantou"
+            ),
             strictOrdering = true
           )
         }
         assertJsonResponse(routes, path = s"$rootPath/images?query=focaccia") {
-          Status.OK -> newImagesListResponse(ids = List("images.examples.bread-focaccia"))
+          Status.OK -> newImagesListResponse(
+            ids = List("images.examples.bread-focaccia")
+          )
         }
     }
   }
@@ -95,21 +103,23 @@ class ImagesTest extends ApiImagesTestBase {
           imagesIndex,
           "images.examples.bread-baguette",
           "images.examples.bread-focaccia",
-          "images.examples.bread-schiacciata",
+          "images.examples.bread-schiacciata"
         )
 
         assertJsonResponse(routes, s"$rootPath/images?query=bread") {
           Status.OK -> newImagesListResponse(
-            ids = List("images.examples.bread-schiacciata", "images.examples.bread-baguette",
-              "images.examples.bread-focaccia"),
+            ids = List(
+              "images.examples.bread-schiacciata",
+              "images.examples.bread-baguette",
+              "images.examples.bread-focaccia"
+            ),
             strictOrdering = true
           )
         }
 
         assertJsonResponse(routes, s"$rootPath/images?query=schiacciata") {
           Status.OK -> newImagesListResponse(
-            ids = List(
-              "images.examples.bread-schiacciata")
+            ids = List("images.examples.bread-schiacciata")
           )
         }
     }
