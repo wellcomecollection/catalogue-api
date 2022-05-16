5cc2a093c8cee1929624a8cac725c11064768827
Alex Chan <alex@alexwlchan.net>
2022-05-16 15:01:49 +0100

    Remove a bunch of now-unused code from the display lib

--- common/display/src/main/scala/weco/catalogue/display_model/Implicits.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/Implicits.scala
@@ -11,33 +11,6 @@ import weco.catalogue.display_model.work._
 
 object Implicits {
 
-  // Circe wants to add a type discriminator, and we don't want it to!  Doing so
-  // would expose internal names like "DisplayDigitalLocationV1" in the public JSON.
-  // So instead we have to do the slightly less nice thing of encoding all the subclasses
-  // here by hand.  Annoying, it is.
-  //
-  // It is the "recommended: approach in the Circe docs:
-  // https://circe.github.io/circe/codecs/adt.html
-
-  implicit val abstractAgentEncoder: Encoder[DisplayAbstractAgent] = {
-    case agent: DisplayAgent               => agent.asJson
-    case person: DisplayPerson             => person.asJson
-    case organisation: DisplayOrganisation => organisation.asJson
-    case meeting: DisplayMeeting           => meeting.asJson
-  }
-
-  implicit val abstractConceptEncoder: Encoder[DisplayAbstractConcept] = {
-    case concept: DisplayConcept => concept.asJson
-    case place: DisplayPlace     => place.asJson
-    case period: DisplayPeriod   => period.asJson
-  }
-
-  implicit val abstractRootConceptEncoder
-    : Encoder[DisplayAbstractRootConcept] = {
-    case agent: DisplayAbstractAgent     => agent.asJson
-    case concept: DisplayAbstractConcept => concept.asJson
-  }
-
   implicit val locationEncoder: Encoder[DisplayLocation] = {
     case digitalLocation: DisplayDigitalLocation =>
       digitalLocation.asJson
@@ -64,22 +37,12 @@ object Implicits {
   // re-derived every time they are required).
 
   implicit val _enc00: Encoder[DisplayAccessCondition] = deriveConfiguredEncoder
-  implicit val _enc01: Encoder[DisplayLanguage] = deriveConfiguredEncoder
   implicit val _enc02: Encoder[DisplayFormat] = deriveConfiguredEncoder
-  implicit val _enc03: Encoder[DisplayPeriod] = deriveConfiguredEncoder
-  implicit val _enc04: Encoder[DisplayContributor] = deriveConfiguredEncoder
   implicit val _enc05: Encoder[DisplayIdentifier] = deriveConfiguredEncoder
-  implicit val _enc06: Encoder[DisplaySubject] = deriveConfiguredEncoder
-  implicit val _enc07: Encoder[DisplayGenre] = deriveConfiguredEncoder
   implicit val _enc09: Encoder[DisplayItem] = deriveConfiguredEncoder
 
   implicit val _dec00: Decoder[DisplayAccessCondition] = deriveConfiguredDecoder
-  implicit val _dec01: Decoder[DisplayLanguage] = deriveConfiguredDecoder
   implicit val _dec02: Decoder[DisplayFormat] = deriveConfiguredDecoder
-  implicit val _dec03: Decoder[DisplayPeriod] = deriveConfiguredDecoder
-  implicit val _dec04: Decoder[DisplayContributor] = deriveConfiguredDecoder
   implicit val _dec05: Decoder[DisplayIdentifier] = deriveConfiguredDecoder
-  implicit val _dec06: Decoder[DisplaySubject] = deriveConfiguredDecoder
-  implicit val _dec07: Decoder[DisplayGenre] = deriveConfiguredDecoder
   implicit val _dec09: Decoder[DisplayItem] = deriveConfiguredDecoder
 }
