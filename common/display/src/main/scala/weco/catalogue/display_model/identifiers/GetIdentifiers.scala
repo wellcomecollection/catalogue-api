f3a18aa3d1d5779d652bbd33d0a65ef95337e122
Alex Chan <alex@alexwlchan.net>
2022-05-17 22:46:15 +0100

    Remove an unused method on GetIdentifiers

--- common/display/src/main/scala/weco/catalogue/display_model/identifiers/GetIdentifiers.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/identifiers/GetIdentifiers.scala
@@ -5,13 +5,4 @@ import weco.catalogue.internal_model.identifiers.IdState
 trait GetIdentifiers {
   protected def getIdentifiers(id: IdState): List[DisplayIdentifier] =
     id.allSourceIdentifiers.map(DisplayIdentifier(_))
-
-  protected def getIdentifiers(
-    id: IdState,
-    includesIdentifiers: Boolean
-  ): Option[List[DisplayIdentifier]] =
-    if (includesIdentifiers)
-      Option(getIdentifiers(id)).filter(_.nonEmpty)
-    else
-      None
 }
