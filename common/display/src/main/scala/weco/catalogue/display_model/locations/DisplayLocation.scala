ebad37a7031a042ad459287d7a938d0acea8dd9a
Alex Chan <alex@alexwlchan.net>
2022-05-18 11:18:25 +0100

    Remove some unused DisplayLocation errors

--- common/display/src/main/scala/weco/catalogue/display_model/locations/DisplayLocation.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/locations/DisplayLocation.scala
@@ -9,16 +9,6 @@ import weco.catalogue.internal_model.locations.{
 
 sealed trait DisplayLocation
 
-object DisplayLocation {
-  def apply(location: Location): DisplayLocation =
-    location match {
-      case digitalLocation: DigitalLocation =>
-        DisplayDigitalLocation(digitalLocation)
-      case physicalLocation: PhysicalLocation =>
-        DisplayPhysicalLocation(physicalLocation)
-    }
-}
-
 case class DisplayDigitalLocation(
   locationType: DisplayLocationType,
   url: String,
@@ -29,19 +19,6 @@ case class DisplayDigitalLocation(
   @JsonKey("type") ontologyType: String = "DigitalLocation"
 ) extends DisplayLocation
 
-object DisplayDigitalLocation {
-  def apply(location: DigitalLocation): DisplayDigitalLocation =
-    DisplayDigitalLocation(
-      locationType = DisplayLocationType(location.locationType),
-      url = location.url,
-      linkText = location.linkText,
-      credit = location.credit,
-      license = location.license.map(DisplayLicense(_)),
-      accessConditions =
-        location.accessConditions.map(DisplayAccessCondition(_))
-    )
-}
-
 case class DisplayPhysicalLocation(
   locationType: DisplayLocationType,
   label: String,
@@ -50,15 +27,3 @@ case class DisplayPhysicalLocation(
   accessConditions: List[DisplayAccessCondition] = Nil,
   @JsonKey("type") ontologyType: String = "PhysicalLocation"
 ) extends DisplayLocation
-
-object DisplayPhysicalLocation {
-  def apply(location: PhysicalLocation): DisplayPhysicalLocation =
-    DisplayPhysicalLocation(
-      locationType = DisplayLocationType(location.locationType),
-      label = location.label,
-      license = location.license.map(DisplayLicense(_)),
-      shelfmark = location.shelfmark,
-      accessConditions =
-        location.accessConditions.map(DisplayAccessCondition(_))
-    )
-}
