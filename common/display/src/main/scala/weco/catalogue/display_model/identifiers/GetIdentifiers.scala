3ad4c0bd9a20bce76f9f7934f2e2a5d8a0bd7462
Alex Chan <alex@alexwlchan.net>
2022-05-18 11:24:57 +0100

    We can remove GetIdentifiers now

--- common/display/src/main/scala/weco/catalogue/display_model/identifiers/GetIdentifiers.scala
+++
@@ -1,8 +0,0 @@
-package weco.catalogue.display_model.identifiers
-
-import weco.catalogue.internal_model.identifiers.IdState
-
-trait GetIdentifiers {
-  protected def getIdentifiers(id: IdState): List[DisplayIdentifier] =
-    id.allSourceIdentifiers.map(DisplayIdentifier(_))
-}
--- common/display/src/main/scala/weco/catalogue/display_model/work/DisplayItem.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/work/DisplayItem.scala
@@ -1,13 +1,8 @@
 package weco.catalogue.display_model.work
 
 import io.circe.generic.extras.JsonKey
-import weco.catalogue.display_model.identifiers.{
-  DisplayIdentifier,
-  GetIdentifiers
-}
+import weco.catalogue.display_model.identifiers.DisplayIdentifier
 import weco.catalogue.display_model.locations.DisplayLocation
-import weco.catalogue.internal_model.identifiers.IdState
-import weco.catalogue.internal_model.work.Item
 
 case class DisplayItem(
   id: Option[String],
