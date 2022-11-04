af314506ef29607002d9fbc133c21cc809f42373
Alex Chan <alex@alexwlchan.net>
2022-11-04 11:53:19 +0000

    Just tell the tests whether we have a docker-compose

--- builds/run_sbt_tests.sh
+++ builds/run_sbt_tests.sh
@@ -6,16 +6,7 @@ set -o verbose
 
 PROJECT="$1"
 
-PROJECT_DIRECTORY=$(jq -r .folder ".sbt_metadata/$PROJECT.json")
-
 ROOT=$(git rev-parse --show-toplevel)
 BUILDS_DIR="$ROOT/builds"
 
-if [[ -f "$PROJECT_DIRECTORY/docker-compose.yml" ]]
-then
-  TEST_COMMAND=";dockerComposeUp;test;dockerComposeStop"
-else
-  TEST_COMMAND="test"
-fi
-
-$BUILDS_DIR/run_sbt_task_in_docker.sh "project $PROJECT" "$TEST_COMMAND"
+$BUILDS_DIR/run_sbt_task_in_docker.sh "project $PROJECT" "test"
