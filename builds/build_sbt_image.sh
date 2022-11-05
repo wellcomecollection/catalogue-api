c92e4d1784ca06e560b2a3e68ac59b7cc4c905ba
Alex Chan <alex@alexwlchan.net>
2022-11-05 09:17:24 +0000

    use IMAGE_TAG in this script

--- builds/build_sbt_image.sh
+++ builds/build_sbt_image.sh
@@ -6,8 +6,7 @@ set -o verbose
 
 PROJECT="$1"
 PROJECT_DIRECTORY="$2"
-
-CURRENT_COMMIT=$(git rev-parse HEAD)
+IMAGE_TAG="$3"
 
 ROOT=$(git rev-parse --show-toplevel)
 BUILDS_DIR="$ROOT/builds"
@@ -16,5 +15,5 @@ $BUILDS_DIR/run_sbt_task_in_docker.sh "project $PROJECT" ";stage"
 
 docker build \
   --file "$PROJECT_DIRECTORY/Dockerfile" \
-  --tag "$PROJECT:$CURRENT_COMMIT" \
+  --tag "$PROJECT:$IMAGE_TAG" \
   "$PROJECT_DIRECTORY"
