55d188553a87b774c49e0d43a812b2babb29ca2e
Alex Chan <alex@alexwlchan.net>
2022-11-09 13:24:16 +0000

    Use the nicer approach to looking up sbt directories
    Co-authored-by: Jamie Parkinson <j.parkinson@wellcome.ac.uk>

--- builds/get_sbt_project_directory.sh
+++ builds/get_sbt_project_directory.sh
@@ -2,7 +2,7 @@
 <<EOF
 Finds the path to an sbt project directory.
 
-This script is mirrored in the catalogue-pipeline and storage-service repos.
+This script is mirrored across our Scala repos.
 
 == Motivation ==
 
@@ -44,42 +44,21 @@ EOF
 set -o errexit
 set -o nounset
 
-PROJECT="$1"
+if (( $# == 1))
+then
+  PROJECT_NAME="$1"
+else
+  echo "Usage: run_sbt_tests.sh <PROJECT>" >&2
+  exit 1
+fi
 
 ROOT=$(git rev-parse --show-toplevel)
-BUILDS_DIR="$ROOT/builds"
 
-# https://stackoverflow.com/a/31236568/1558022
-function relpath() {
-  python3 -c "import os, sys; print(os.path.relpath(*(sys.argv[1:])))" "$@";
-}
-
-function strip_whitespace() {
-  python3 -c "import sys; print(sys.stdin.read().strip())";
-}
-
-# The "show project/baseDirectory" command will return output like
-#
-#     [info] welcome to sbt 1.4.1 (Homebrew Java 16.0.2)
-#     [info] loading global plugins from /Users/alexwlchan/.sbt/1.0/plugins
-#     [info] loading project definition from /Users/alexwlchan/repos/catalogue-api/project/project
-#     [info] loading settings for project catalogue-api-build from build.sbt,plugins.sbt ...
-#     [info] loading project definition from /Users/alexwlchan/repos/catalogue-api/project
-#     [info] loading settings for project catalogue-api from build.sbt ...
-#     [info] set current project to catalogue-api (in build file:/Users/alexwlchan/repos/catalogue-api/)
-#     [info] Running in build environment: dev
-#     [info] Installing the s3:// URLStreamHandler via java.net.URL.setURLStreamHandlerFactory
-#     [info] /Users/alexwlchan/repos/catalogue-api/snapshots/snapshot_generator
-#
-# We want to grab the path from the final line.
-#
-# Note that this ends with a carriage return (\r), which we need to discard.
-#
+# Note that the output from sbt ends with a carriage return (\r), which
+# we need to discard.
 BASE_DIR=$(
-  $BUILDS_DIR/run_sbt_task_in_docker.sh "show $PROJECT/baseDirectory" \
-    | tail -n 1 \
-    | awk '{print $2}' \
-    | strip_whitespace
+  ./builds/run_sbt_task_in_docker.sh -error "show $PROJECT_NAME" "stage; print baseDirectory" \
+    | tr -d '\r'
 )
 
 echo $(relpath "$BASE_DIR" "$ROOT")
