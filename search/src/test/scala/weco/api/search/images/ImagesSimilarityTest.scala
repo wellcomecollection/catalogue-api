33dfaebbe7629309619feb076a26a2c3f4045378
Paul Butcher <paul.butcher@gmail.com>
2022-12-06 11:04:23 +0000

    fix image similarity tests

--- search/src/test/scala/weco/api/search/images/ImagesSimilarityTest.scala
+++ search/src/test/scala/weco/api/search/images/ImagesSimilarityTest.scala
@@ -1,44 +1,65 @@
 package weco.api.search.images
-
+// TODO: These tests don't really do anything, as the actual similarity checking is now in Rank
+// However, we should change them to check whether the appropriate property has been included
+// this will act as a reasonable proxy for checking that the querystring parameter is invoking
+// the similarity search.
 class ImagesSimilarityTest extends ApiImagesTestBase {
-  describe("image similarity"){
+
+  /**
+    * These tests treat the simple presence of the requested property as a proxy
+    * for populating the similar image lists.  Essentially, this is showing the
+    * connection between the `include` querystring parameter and the resulting JSON.
+    *
+    * The thresholds are set too high to be able to actually return similar images in
+    * these dummy indices, so the content of these image lists is irrelevant to the tests.
+    *
+    * Tests that show that the similar images functionality behaves as expected
+    * can be found in search/src/test/scala/weco/api/search/services/ImagesServiceTest.scala
+    * and there should also be rank tests to cover whether the images returned are appropriately similar
+    */
+  describe("image similarity") {
+
     it("includes visually similar images with ?include=visuallySimilar") {
       withImagesApi {
         case (imagesIndex, routes) =>
           indexTestDocuments(
             imagesIndex,
-            (0 to 5).map(i => s"images.similar-features-and-palettes.$i"): _*
+            "images.similar-features-and-palettes.0"
           )
 
-          assertJsonResponse(
+          assertJsonResponseLike(
             routes,
             path =
-              s"$rootPath/images/${getTestImageId("images.similar-features-and-palettes.0")}?include=visuallySimilar"
-          ) {
-            Status.OK -> readResource(
-              "expected_responses/visually-similar-features-and-palettes.json"
-            )
-          }
+              s"$rootPath/images/${getTestImageId("images.similar-features-and-palettes.0")}?include=visuallySimilar",
+            assertion = responseJson => {
+              assert(responseJson.asObject.get.contains("visuallySimilar"))
+              assert(!responseJson.asObject.get.contains("withSimilarFeatures"))
+              assert(!responseJson.asObject.get.contains("withSimilarColors"))
+            }
+          )
       }
     }
 
-    it("includes images with similar features with ?include=withSimilarFeatures") {
+    it(
+      "includes images with similar features with ?include=withSimilarFeatures"
+    ) {
       withImagesApi {
         case (imagesIndex, routes) =>
           indexTestDocuments(
             imagesIndex,
-            (0 to 5).map(i => s"images.similar-features.$i"): _*
+            "images.similar-features.0"
           )
 
-          assertJsonResponse(
+          assertJsonResponseLike(
             routes,
             path =
-              s"$rootPath/images/${getTestImageId("images.similar-features.0")}?include=withSimilarFeatures"
-          ) {
-            Status.OK -> readResource(
-              "expected_responses/visually-similar-features.json"
-            )
-          }
+              s"$rootPath/images/${getTestImageId("images.similar-features.0")}?include=withSimilarFeatures",
+            assertion = responseJson => {
+              assert(responseJson.asObject.get.contains("withSimilarFeatures"))
+              assert(!responseJson.asObject.get.contains("visuallySimilar"))
+              assert(!responseJson.asObject.get.contains("withSimilarColors"))
+            }
+          )
       }
     }
 
@@ -49,18 +70,39 @@ class ImagesSimilarityTest extends ApiImagesTestBase {
         case (imagesIndex, routes) =>
           indexTestDocuments(
             imagesIndex,
-            (0 to 5).map(i => s"images.similar-palettes.$i"): _*
+            "images.similar-palettes.0"
           )
-
-          assertJsonResponse(
+          assertJsonResponseLike(
             routes,
             path =
-              s"$rootPath/images/${getTestImageId("images.similar-palettes.0")}?include=withSimilarColors"
-          ) {
-            Status.OK -> readResource(
-              "expected_responses/visually-similar-palettes.json"
-            )
-          }
+              s"$rootPath/images/${getTestImageId("images.similar-palettes.0")}?include=withSimilarColors",
+            assertion = responseJson => {
+              assert(responseJson.asObject.get.contains("withSimilarColors"))
+              assert(!responseJson.asObject.get.contains("visuallySimilar"))
+              assert(!responseJson.asObject.get.contains("withSimilarFeatures"))
+            }
+          )
+      }
+    }
+
+    it("includes all requested similarity fields in a comma-separated list") {
+      withImagesApi {
+        case (imagesIndex, routes) =>
+          indexTestDocuments(
+            imagesIndex,
+            "images.similar-features-and-palettes.0"
+          )
+
+          assertJsonResponseLike(
+            routes,
+            path =
+              s"$rootPath/images/${getTestImageId("images.similar-features-and-palettes.0")}?include=visuallySimilar,withSimilarFeatures,withSimilarColors",
+            assertion = responseJson => {
+              assert(responseJson.asObject.get.contains("visuallySimilar"))
+              assert(responseJson.asObject.get.contains("withSimilarFeatures"))
+              assert(responseJson.asObject.get.contains("withSimilarColors"))
+            }
+          )
       }
     }
 
