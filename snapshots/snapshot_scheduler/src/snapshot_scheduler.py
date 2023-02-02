34151d0c57b8ab59200b91b19112a7704072e032
jamie <j.parkinson@wellcome.ac.uk>
2023-02-02 14:13:51 +0000

    Snapshot scheduler uses prod API indices and populates SnapshotJob to match what the generator expects

--- snapshots/snapshot_scheduler/src/snapshot_scheduler.py
+++ snapshots/snapshot_scheduler/src/snapshot_scheduler.py
@@ -2,52 +2,38 @@
 Publish a new SnapshotJob to SNS.
 """
 
-import datetime
 import os
 
 import attr
 import boto3
+import httpx
 from wellcome_aws_utils.lambda_utils import log_on_error
 from wellcome_aws_utils.sns_utils import publish_sns_message
 
-
-@attr.s
-class S3ObjectLocation:
-    bucket = attr.ib()
-    key = attr.ib()
+from snapshot_job import get_snapshot_jobs
 
 
-# This class is duplicated in the snapshot generator app
-# Changes here will need to be reflected there, and vice versa.
-@attr.s
-class SnapshotJob(object):
-    s3Location = attr.ib(type=S3ObjectLocation)
-    apiVersion = attr.ib(type=str)
-    requestedAt = attr.ib(type=datetime.datetime)
+def get_current_api_indices():
+    response = httpx.get("https://api.wellcomecollection.org/catalogue/v2/_elasticConfig")
+    response_data = response.json()
+    return {
+        "works": response_data["worksIndex"],
+        "images": response_data["imagesIndex"]
+    }
 
 
 @log_on_error
-def main(event=None, _ctxt=None, sns_client=None):
+def main(event=None, _ctxt=None, sns_client=None, indices=None):
     print(os.environ)
     sns_client = sns_client or boto3.client("sns")
+    indices = indices or get_current_api_indices()
 
-    topic_arn = os.environ["TOPIC_ARN"]
-
-    public_bucket_name = os.environ["PUBLIC_BUCKET_NAME"]
-    public_object_key_v2 = os.environ["PUBLIC_OBJECT_KEY_V2"]
-
-    for (api_version, public_object_key) in [("v2", public_object_key_v2)]:
-        snapshot_job = SnapshotJob(
-            s3Location=S3ObjectLocation(
-                bucket=public_bucket_name, key=public_object_key_v2
-            ),
-            apiVersion=api_version,
-            requestedAt=datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-        )
-
+    snapshot_jobs = get_snapshot_jobs(indices)
+    topic_arn = os.getenv("TOPIC_ARN")
+    for job in snapshot_jobs:
         publish_sns_message(
             sns_client=sns_client,
             topic_arn=topic_arn,
-            message=attr.asdict(snapshot_job),
+            message=attr.asdict(job),
             subject="source: snapshot_scheduler.main",
         )
