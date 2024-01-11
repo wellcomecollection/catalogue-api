cccd866d34760bbec139fb4988552ca38e0d0b59
Robert Kenny <r.kenny@wellcome.org>
2024-01-11 17:10:12 +0000

    Add option for HTTP healthcheck, enable for works

--- terraform/modules/service/target_group.tf
+++ terraform/modules/service/target_group.tf
@@ -15,8 +15,21 @@ resource "aws_lb_target_group" "tcp" {
   # updated service.  Reducing this parameter to 90s makes deployments faster.
   deregistration_delay = 90
 
-  health_check {
-    protocol = "TCP"
+  dynamic "health_check" {
+    for_each = var.tcp_healthcheck == true ? toset([]) : toset([1])
+
+    content {
+      protocol = "TCP"
+    }
+  }
+
+  dynamic "health_check" {
+    for_each = var.tcp_healthcheck == false ? toset([]) : toset([1])
+
+    content {
+      protocol = "HTTP"
+      path = var.healthcheck_path
+    }
   }
 }
 
