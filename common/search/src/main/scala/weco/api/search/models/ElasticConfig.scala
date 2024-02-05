4c50c150d012445f2705e628b3bc72bd6da8c7bc
Paul Butcher <paul.butcher@gmail.com>
2024-02-05 14:51:46 +0000

    new pipeline 2024-02-01

--- common/search/src/main/scala/weco/api/search/models/ElasticConfig.scala
+++ common/search/src/main/scala/weco/api/search/models/ElasticConfig.scala
@@ -10,7 +10,7 @@ case class ElasticConfig(
 trait ElasticConfigBase {
   // We use this to share config across Scala API applications
   // i.e. The API and the snapshot generator.
-  val pipelineDate = "2024-01-09"
+  val pipelineDate = "2024-02-01"
 }
 
 object PipelineClusterElasticConfig extends ElasticConfigBase {
