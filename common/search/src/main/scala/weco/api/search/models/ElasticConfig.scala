46401405603dde820bed78c7a1952453f1da05d3
jamie <j.parkinson@wellcome.org>
2024-02-21 09:53:57 +0000

    Bump pipeline date to 02-19

--- common/search/src/main/scala/weco/api/search/models/ElasticConfig.scala
+++ common/search/src/main/scala/weco/api/search/models/ElasticConfig.scala
@@ -10,7 +10,7 @@ case class ElasticConfig(
 trait ElasticConfigBase {
   // We use this to share config across Scala API applications
   // i.e. The API and the snapshot generator.
-  val pipelineDate = "2024-02-01"
+  val pipelineDate = "2024-02-19"
 }
 
 object PipelineClusterElasticConfig extends ElasticConfigBase {
