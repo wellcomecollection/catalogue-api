3e2a5a5cf10747267793af66b7c89875d6952b41
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-04-30 11:13:14 +0000

    Apply auto-formatting rules

--- items/src/main/scala/weco/api/items/services/SierraItemUpdater.scala
+++ items/src/main/scala/weco/api/items/services/SierraItemUpdater.scala
@@ -66,9 +66,11 @@ class SierraItemUpdater(
     for {
       itemEither <- sierraSource.lookupItemEntries(existingItems.keys.toSeq)
 
-      maybeAccessConditions: Map[SierraItemNumber, Option[
-        DisplayAccessCondition
-      ]] = itemEither match {
+      maybeAccessConditions: Map[
+        SierraItemNumber,
+        Option[
+          DisplayAccessCondition
+        ]] = itemEither match {
         case Right(SierraItemDataEntries(_, _, entries)) =>
           entries
             .map(item => {
