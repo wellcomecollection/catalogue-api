c57317ae7c7b505a92534fee8da13c05f8c5894e
Robert Kenny <r.kenny@wellcome.org>
2024-05-01 11:16:36 +0100

    add test, move isdev to function on apiconfig

--- common/search/src/main/scala/weco/api/search/models/ApiConfig.scala
+++ common/search/src/main/scala/weco/api/search/models/ApiConfig.scala
@@ -9,25 +9,24 @@ case class ApiConfig(
   publicHost: String,
   publicRootPath: String,
   defaultPageSize: Int,
+) {
   // Used to determine whether we're running in a dev environment
-  isDev: Boolean
-)
+  def isDev = publicHost == "localhost"
+}
 
 object ApiConfig {
   private val defaultRootUri = "https://api.wellcomecollection.org/catalogue/v2"
 
   def build(config: Config): ApiConfig = {
-    val apiPublicRoot = config
-      .getStringOption("api.public-root")
-      .getOrElse(defaultRootUri)
-
     ApiConfig(
-      publicRootUri = Uri(apiPublicRoot),
+      publicRootUri = Uri(
+        config
+          .getStringOption("api.public-root")
+          .getOrElse(defaultRootUri)
+      ),
       defaultPageSize = config
         .getIntOption("api.pageSize")
         .getOrElse(10),
-      // Infer whether we're running in a dev environment from the public root
-      isDev = apiPublicRoot.contains("localhost")
     )
   }
 
@@ -41,6 +40,5 @@ object ApiConfig {
       publicScheme = publicRootUri.scheme,
       publicRootPath = publicRootUri.path.toString,
       defaultPageSize = defaultPageSize,
-      isDev = isDev
     )
 }
--- common/search/src/test/scala/weco/api/search/models/ApiConfigTest.scala
+++ common/search/src/test/scala/weco/api/search/models/ApiConfigTest.scala
@@ -14,10 +14,27 @@ class ApiConfigTest extends AnyFunSpec with Matchers with Inside {
         defaultPageSize = 10
       )
     ) {
-      case ApiConfig(publicScheme, publicHost, publicRootPath, _) =>
+      case apiConfig@ApiConfig(publicScheme, publicHost, publicRootPath, _) =>
         publicScheme shouldBe "https"
         publicHost shouldBe "api.wellcomecollection.org"
         publicRootPath shouldBe "/catalogue/v2"
+        apiConfig.isDev shouldBe false
+    }
+  }
+
+  it("correctly identifies a dev environment") {
+    val publicRoot = "https://localhost:8080/catalogue/v2"
+    inside(
+      ApiConfig(
+        publicRootUri = Uri(publicRoot),
+        defaultPageSize = 10
+      )
+    ) {
+      case apiConfig@ApiConfig(publicScheme, publicHost, publicRootPath, _) =>
+        publicScheme shouldBe "https"
+        publicHost shouldBe "localhost"
+        publicRootPath shouldBe "/catalogue/v2"
+        apiConfig.isDev shouldBe true
     }
   }
 }
