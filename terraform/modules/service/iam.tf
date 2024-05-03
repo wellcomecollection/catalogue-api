4aa167f8c65571d9cfd4f6a231a798940cb26a5f
Robert Kenny <r.kenny@wellcome.org>
2024-05-03 09:47:56 +0100

    Allow catalogue services to read from stacks secret space

--- terraform/modules/service/iam.tf
+++ terraform/modules/service/iam.tf
@@ -11,6 +11,7 @@ data "aws_iam_policy_document" "get_secrets" {
 
     resources = [
       "arn:aws:secretsmanager:eu-west-1:756629837203:secret:elasticsearch/*",
+      "arn:aws:secretsmanager:eu-west-1:756629837203:secret:stacks/*",
     ]
   }
 }
