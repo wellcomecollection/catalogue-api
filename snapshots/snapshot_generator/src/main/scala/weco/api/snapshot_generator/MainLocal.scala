f4ff9bfaabb8de3d3971672460b60033ba0aae0c
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-03-10 13:45:40 +0000

    Apply auto-formatting rules

--- snapshots/snapshot_generator/src/main/scala/weco/api/snapshot_generator/MainLocal.scala
+++ snapshots/snapshot_generator/src/main/scala/weco/api/snapshot_generator/MainLocal.scala
@@ -6,7 +6,6 @@ import weco.api.search.models.ApiEnvironment
 import weco.api.snapshot_generator.models.SnapshotJob
 import weco.api.snapshot_generator.services.SnapshotService
 import weco.storage.providers.s3.S3ObjectLocation
-import com.sksamuel.elastic4s.Index
 import com.typesafe.config.{Config, ConfigFactory}
 
 import java.time.Instant
@@ -17,24 +16,24 @@ object MainLocal {
 
     val snapshotService =
       new SnapshotService(
-        PipelineElasticClientBuilder("snapshot_generator", _, ApiEnvironment.Dev),
+        PipelineElasticClientBuilder(
+          "snapshot_generator",
+          _,
+          ApiEnvironment.Dev),
         S3Client.builder().build()
       )
 
-
     val snapshotJob = SnapshotJob(
-      s3Location=S3ObjectLocation(
-        bucket=config.getString("snapshotBucketName"),
-        key=config.getString("snapshotBucketKey")
+      s3Location = S3ObjectLocation(
+        bucket = config.getString("snapshotBucketName"),
+        key = config.getString("snapshotBucketKey")
       ),
-      requestedAt=Instant.now(),
-      pipelineDate=config.getString("pipelineDate"),
-      index=config.getString("snapshotIndex"),
-      bulkSize=150,
-      query=Option(config.getString("snapshotQuery"))
-    );
-    val completedSnapshotJob = snapshotService.generateSnapshot(snapshotJob);
-
-    println(completedSnapshotJob);
+      requestedAt = Instant.now(),
+      pipelineDate = config.getString("pipelineDate"),
+      index = config.getString("snapshotIndex"),
+      bulkSize = 150,
+      query = Option(config.getString("snapshotQuery"))
+    )
+    snapshotService.generateSnapshot(snapshotJob);
   }
 }
