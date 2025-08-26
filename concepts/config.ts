785b0ffdb43276490192160494f60b726171d68d
Robert Kenny <r.kenny@wellcome.org>
2025-08-26 11:13:44 +0100

    Use pipeline 2025-08-14

--- concepts/config.ts
+++ concepts/config.ts
@@ -10,8 +10,8 @@ const environmentSchema = z.object({
 const environment = environmentSchema.parse(process.env);
 
 const config = {
-  pipelineDate: "2025-05-01",
-  conceptsIndex: "concepts-indexed-2025-07-21",
+  pipelineDate: "2025-08-14",
+  conceptsIndex: "concepts-indexed-2025-08-21",
   publicRootUrl: new URL(environment.PUBLIC_ROOT_URL),
 };
 
