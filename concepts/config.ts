3bbf2da251e5a5745421853b56ad49052bec75c1
Agnes Garoux <a.garoux@wellcome.ac.uk>
2025-10-14 13:44:34 +0100

    use concepts-indexed-2025-10-14

--- concepts/config.ts
+++ concepts/config.ts
@@ -11,7 +11,7 @@ const environment = environmentSchema.parse(process.env);
 
 const config = {
   pipelineDate: "2025-08-14",
-  conceptsIndex: "concepts-indexed-2025-10-10",
+  conceptsIndex: "concepts-indexed-2025-10-14",
   publicRootUrl: new URL(environment.PUBLIC_ROOT_URL),
 };
 
