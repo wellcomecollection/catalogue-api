0a49e7a15153a93833b7c5be7181d4b4a66ecf65
Robert Kenny <r.kenny@wellcome.org>
2025-11-20 11:42:40 +0000

    add snapshot generator tests

--- snapshots/snapshot_generator/src/test/scala/weco/api/snapshot_generator/fixtures/SnapshotServiceFixture.scala
+++ snapshots/snapshot_generator/src/test/scala/weco/api/snapshot_generator/fixtures/SnapshotServiceFixture.scala
@@ -2,14 +2,16 @@ package weco.api.snapshot_generator.fixtures
 
 import com.sksamuel.elastic4s.{ElasticClient, Index}
 import org.scalatest.Suite
+import weco.api.search.elasticsearch.ResilientElasticClient
 import weco.api.search.fixtures.IndexFixtures
-import weco.api.snapshot_generator.models.SnapshotJob
+import weco.api.snapshot_generator.models.{PipelineElasticClient, SnapshotJob}
 import weco.api.snapshot_generator.services.SnapshotService
 import weco.fixtures.TestWith
 import weco.storage.fixtures.S3Fixtures
 import weco.storage.providers.s3.S3ObjectLocation
 
 import java.time.Instant
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait SnapshotServiceFixture extends IndexFixtures with S3Fixtures {
   this: Suite =>
@@ -33,7 +35,13 @@ trait SnapshotServiceFixture extends IndexFixtures with S3Fixtures {
     elasticClient: ElasticClient = elasticClient
   )(testWith: TestWith[SnapshotService, R]): R =
     testWith(
-      new SnapshotService(_ => elasticClient, s3Client)
+      new SnapshotService(
+        new PipelineElasticClient {
+          override def forDate(pipelineDate: String): ResilientElasticClient =
+            new ResilientElasticClient(() => elasticClient)
+        },
+        s3Client
+      )
     )
 }
 
