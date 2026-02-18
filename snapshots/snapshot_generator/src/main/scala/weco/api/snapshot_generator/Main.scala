dba2d025c18c4c0beed0ccf51d478383d5ffdb73
Štěpán Brychta <stepan.brychta@me.com>
2026-02-18 10:15:57 +0000

    Fix snapshot generator

--- snapshots/snapshot_generator/src/main/scala/weco/api/snapshot_generator/Main.scala
+++ snapshots/snapshot_generator/src/main/scala/weco/api/snapshot_generator/Main.scala
@@ -4,10 +4,9 @@ import org.apache.pekko.actor.ActorSystem
 import com.typesafe.config.Config
 import software.amazon.awssdk.services.s3.S3Client
 import weco.api.search.config.builders.PipelineElasticClientBuilder
-import weco.api.snapshot_generator.services.{
-  SnapshotGeneratorWorkerService,
-  SnapshotService
-}
+import weco.api.search.models.ElasticConfig
+import weco.api.snapshot_generator.models.PipelineElasticClient
+import weco.api.snapshot_generator.services.{SnapshotGeneratorWorkerService, SnapshotService}
 import weco.messaging.sns.NotificationMessage
 import weco.messaging.typesafe.{SNSBuilder, SQSBuilder}
 import weco.typesafe.WellcomeTypesafeApp
@@ -23,9 +22,17 @@ object Main extends WellcomeTypesafeApp {
 
     val s3Client: S3Client = S3Client.builder().build()
 
+    val pipelineElasticClient: PipelineElasticClient =
+      (pipelineDate: String) => PipelineElasticClientBuilder(
+        ElasticConfig.forDefaultCluster(
+          serviceName = "snapshot_generator",
+          pipelineDate = pipelineDate
+        )
+      )
+
     val snapshotService =
       new SnapshotService(
-        PipelineElasticClientBuilder("snapshot_generator", _),
+        pipelineElasticClient,
         s3Client
       )
 
