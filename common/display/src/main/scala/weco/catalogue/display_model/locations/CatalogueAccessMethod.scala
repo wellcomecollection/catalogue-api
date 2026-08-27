25be19311ef14f4e00caeb3c41184f04b0ffc8f2
Štěpán Brychta <stepan.brychta@me.com>
2026-08-27 09:02:22 +0100

    Update CatalogueAccessMethod.scala

--- common/display/src/main/scala/weco/catalogue/display_model/locations/CatalogueAccessMethod.scala
+++ common/display/src/main/scala/weco/catalogue/display_model/locations/CatalogueAccessMethod.scala
@@ -2,7 +2,7 @@ package weco.catalogue.display_model.locations
 
 object CatalogueAccessMethod {
   // These values mirror the access methods from the catalogue pipeline
-  // See https://github.com/wellcomecollection/catalogue-pipeline/blob/main/common/internal_model/src/main/scala/weco/catalogue/internal_model/locations/AccessMethod.scala
+  // See https://github.com/wellcomecollection/catalogue-pipeline/blob/main/catalogue_graph/src/ingestor/models/display/access_method.py
   //
   // Only the methods the API needs to name are given as id/label pairs below
   val OnlineRequest = DisplayAccessMethod(
