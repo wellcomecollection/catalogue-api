a543bf30a38541c1cb6be74f1d756420c1903267
Robert Kenny <r.kenny@wellcome.org>
2026-09-11 09:28:57 +0100

    Retag environments from the build's own ref tag instead of latest

--- builds/update_ecr_image_tag.sh
+++ builds/update_ecr_image_tag.sh
@@ -22,7 +22,9 @@ We have three types of tag:
       These are the tags used in our ECS task definitions.
 
 This script updates the env tags of one or more repositories to point at
-whatever 'latest' is.
+whatever 'latest' is, or at the tag named by LATEST_TAG if set. Deploys set
+LATEST_TAG to the build's own ref tag so an environment runs the commit the
+build was for, even if newer images have been published since.
 
 == Worked example ==
 
@@ -62,22 +64,35 @@ pointed at by 'env.prod' will be unchanged:
         More generally you can supply an arbitrary number of ECR repos as
         additional arguments, and they will all be retagged.
 
+    ENV_TAG="env.prod" LATEST_TAG="ref.8656d3f" update_ecr_image_tag.sh search
+
+        This will update the 'env.prod' tag in the search ECR repository to
+        point to the image tagged 'ref.8656d3f', failing if there is none.
+
 EOF
 
 set -o errexit
 set -o nounset
 
+LATEST_TAG=${LATEST_TAG:-latest}
+
 for repositoryName in "$@"
 do
   # These commands are based on an AWS ECR guide:
   # https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-retag.html
-  echo "Updating the $ENV_TAG tag in $repositoryName"
+  echo "Updating the $ENV_TAG tag in $repositoryName to $LATEST_TAG"
   LATEST_MANIFEST=$(
     aws ecr batch-get-image \
       --repository-name "$repositoryName" \
-      --image-ids imageTag=latest --output json \
+      --image-ids imageTag="$LATEST_TAG" --output json \
       | jq --raw-output --join-output '.images[0].imageManifest'
   )
+
+  if [[ "$LATEST_MANIFEST" == "null" ]]
+  then
+    echo "No image tagged $LATEST_TAG in $repositoryName" >&2
+    exit 1
+  fi
 
   TAGGED_MANIFEST=$(
     aws ecr batch-get-image \
